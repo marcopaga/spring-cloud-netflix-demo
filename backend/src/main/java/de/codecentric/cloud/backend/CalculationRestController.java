50322016e74d1b1be97d32b2f245239f9a58563a
Marco Paga <marco.paga@codecentric.de>
2017-07-24 15:18:01 +0200

    Dockerized and using Docker Compose

--- backend/src/main/java/de/codecentric/cloud/backend/CalculationRestController.java
+++ backend/src/main/java/de/codecentric/cloud/backend/CalculationRestController.java
@@ -1,11 +1,17 @@
 package de.codecentric.cloud.backend;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 public class CalculationRestController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CalculationRestController.class);
 
     @GetMapping("/")
     public String index(){
@@ -14,6 +20,25 @@ public class CalculationRestController {
 
     @GetMapping("/add/{first}/{second}/")
     public int add(@PathVariable("first") int first, @PathVariable("second") int second){
-        return first + second;
+        injectFailure();
+        simulateLongProcessing();
+        final int result = first + second;
+        LOGGER.info("Calculation of {} + {} = {}.", first, second, result);
+        return result;
+    }
+
+    private void simulateLongProcessing() {
+        final int processingTime = new Random().nextInt(1100);
+        LOGGER.info("Processing will be delayed by {} ms.", processingTime);
+        try {
+            Thread.sleep(processingTime);
+        } catch (InterruptedException ignored) {
+        }
+    }
+
+    private void injectFailure() {
+        if(new Random().nextInt(100) > 95){
+            throw new IllegalStateException("This error was automatically created for you to test this stack.");
+        }
     }
 }
--- frontend/src/main/java/de/codecentric/cloud/frontend/FrontendRestController.java
+++ frontend/src/main/java/de/codecentric/cloud/frontend/FrontendRestController.java
@@ -1,5 +1,7 @@
 package de.codecentric.cloud.frontend;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,13 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
-
 import java.util.Random;
 
 import static java.lang.String.format;
 
 @RestController
 public class FrontendRestController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FrontendRestController.class);
 
     @Resource
     public RestTemplate restTemplate;
@@ -23,13 +26,19 @@ public class FrontendRestController {
         final Random random = new Random();
         int first = random.nextInt();
         int second = random.nextInt();
-        final ResponseEntity<Integer> result = restTemplate.getForEntity("http://backend/add/{first}/{second}/", Integer.class, first, second);
-        return format("%d + %d = %d", first, second, result.getBody());
+
+        return performAddition(first, second);
     }
 
     @GetMapping("/calculate/{first}/{second}")
     public String calculate(@PathVariable("first") int first, @PathVariable("second") int second){
+        return performAddition(first, second);
+    }
+
+    private String performAddition(int first, int second) {
+        LOGGER.info("Calculation of {} + {} requested.", first, second);
         final ResponseEntity<Integer> result = restTemplate.getForEntity("http://backend/add/{first}/{second}/", Integer.class, first, second);
+        LOGGER.info("Result is: {}", result.getBody());
         return format("%d + %d = %d", first, second, result.getBody());
     }
 }
