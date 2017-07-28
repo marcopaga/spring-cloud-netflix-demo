c945116bb28f440d6884cb6d1fa65d2a61987167
Marco Paga <marco.paga@codecentric.de>
2017-07-28 11:41:23 +0200

    Hystrix activated in the frontend

--- frontend/src/main/java/de/codecentric/cloud/frontend/FrontendApplication.java
+++ frontend/src/main/java/de/codecentric/cloud/frontend/FrontendApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableHystrix
 public class FrontendApplication {
 
 	@Bean
